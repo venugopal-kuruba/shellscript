09c01e0a6d8dd786672d3f18a61a34cd12a44d01
kuruba venu gopal <kurubavenugopal1435@gmail.com>
2024-08-13 21:53:50 +0530

    args set into short

--- args.sh
+++ args.sh
@@ -1,8 +1,6 @@
-args1=$1
-args2=$2
-args3=$3
+#!/bin/bash
 
 echo "actor number tags 0:$0"
-echo "actor number tags 1:$args1"
-echo "actor number tags 2:$args2"
-echo "actor number tags 3:$args3"
+echo "actor number tags 1:$1"
+echo "actor number tags 2:$2"
+echo "actor number tags 3:$3"
