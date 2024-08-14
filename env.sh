337f98797be93ba31cb1e82a2262af1db4e28f15
kuruba venu gopal <kurubavenugopal1435@gmail.com>
2024-08-14 14:28:09 +0530

    script to get vpcs using jq

--- env.sh
+++ env.sh
@@ -1,15 +1,3 @@
-#dev-account
-
-#export AWS_Access key:AKIA3FLDXA3MQ2ESVH7M
-#export AWS_Secret access key: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
-
-
-#prod account
-
-#export AWS_Access key:AKIA6GBMHSALO23HKOLP
-#export AWS_Secret access key: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
-
-
 #!/bin/bash
  region=$1
  echo "retriving vpcs and vpcs id in ${region}.."
