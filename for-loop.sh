7d58670c8cfaed52c6fc84d573b4c5ee37bd866c
kuruba venu gopal <kurubavenugopal1435@gmail.com>
2024-08-15 22:21:06 +0530

    for loop

--- for-loop.sh
+++ for-loop.sh
@@ -1,16 +1,35 @@
 #!/bin/bash
 iteams=$@
-echo $iteams
+#echo $iteams
 
 for item in $iteams
 do
-    echo $item
+    #echo $item
+    command -v $item > /dev/null
+    if [ $? -eq 0 ]
+    then 
+        echo " $item is present "
+    else
+        echo " $item is not present "
+    fi
 done
 
-#command -v $1 > /dev/null
-#if [ $? -eq 0 ]
-#then 
- #   echo " $1 is present "
-#else
-#    echo " $1 is not present "
-#fi
+
+echo ''
+echo ''
+
+
+iteams2=$*     # same function both $* and $@
+#echo $iteams
+
+for item2 in $iteams2
+do
+    #echo $item
+    command -v $item2 > /dev/null
+    if [ $? -eq 0 ]
+    then 
+        echo " $item2 is present "
+    else
+        echo " $item2 is not present "
+    fi
+done
