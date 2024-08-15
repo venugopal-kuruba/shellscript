5d80b03fffc3b3e27afe20879994a06c25e2c57a
kuruba venu gopal <kurubavenugopal1435@gmail.com>
2024-08-15 17:48:08 +0530

    to know about /dev/null and tee and cut

---
+++ std_out_error.sh
@@ -0,0 +1,13 @@
+Standard Input -0  
+Standard output -1 
+Standard error -2  
+
+#!/bin/bash
+
+uname  -a               #type of operation system
+venu                    # to get error
+free -h                 # to get how much memory was free
+vikram anna             # to get error
+cat /etc/lsb-release  
+ganesh
+
