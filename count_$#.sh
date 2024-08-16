a021c9400054d77aefa32ce68155f78e8d488bbe
kuruba venu gopal <kurubavenugopal1435@gmail.com>
2024-08-16 12:13:44 +0530

    aruments counting

---
+++ count_$#.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "$# arguments passed to the script"
+
+if [ $# -gt 0 ]
+then 
+    for item in $@
+    do
+        command -v $item > /dev/null
+        if [ $? -eq 0 ]
+            echo "$item is present"
+        else
+            echo "$item is not present"
+        fi
+    done
+else
+    echo "please enter the arguments"
+fi   
