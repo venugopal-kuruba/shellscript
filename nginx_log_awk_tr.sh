a28ba97690168d1d4eae3a2419b544d272af0bd8
kuruba venu gopal <kurubavenugopal1435@gmail.com>
2024-08-17 12:53:11 +0530

    parsing usind awk and tr

---
+++ nginx_log_awk_tr.sh
@@ -0,0 +1,13 @@
+#file name is /tmp/nginx.log
+
+# i need all ips ,with 404 error and path
+cat /tmp/nginx.log | awk -F " " '{print $1,$9,$7}'
+
+#you will get aoutput of ips ,with 404 error and path but it comes with space.
+
+# i need to replace the space with the 
+cat /tmp/nginx.log | awk -F " " '{print $1,$9,$7}' | tr " " ","
+
+# i need to rremove the / from yhe data and store in a file (/tmp/dat.csv)
+cat /tmp/nginx.log | awk -F " " '{print $1,$9,$7}' | tr " " "," | tr -d "/" > /tmp/dat.csv
+
