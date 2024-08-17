e3eb8f89122c2066599a191f417ad7bf6220e5c7
kuruba venu gopal <kurubavenugopal1435@gmail.com>
2024-08-17 11:46:15 +0530

    min and max by using find

--- min_max_size.sh
+++ min_max_size.sh
@@ -3,10 +3,10 @@
 read -p "Enter the minimum size:" MIN_SIZE
 read -p "Enter the maximum size:" MAX_SIZE
 read -p "Enter the path:" file_path
-find $file_path -name "*.log" -size +${MIN_SIZE}M -size -${MAX_SIZE}M | xargs du -ch
+find $file_path -name "*.zip" -size +${MIN_SIZE}M -size -${MAX_SIZE}M | xargs du -ch
 
                             #or
 
 #!bin/bash
 
-find $1 -name "*.log" -size +${2}M -size -${3}M | xargs du -ch
+find $1 -name "*.zip" -size +${2}M -size -${3}M | xargs du -ch
